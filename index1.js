289b07fd0eb9d7c87bf94de9c477f6dc163f79be
Ikromjon <ikromjon98.98@icloud.com>
2022-06-13 20:42:01 +0200

    added another type of solution

--- index1.js
+++ index1.js
@@ -1,32 +1,23 @@
-const solve = (n1, operator, n2) => {
-    const a = parseFloat(n1);
-    const b = parseFloat(n2);
-    if (operator === 'add') return a + b;
-    if (operator === 'subtract') return a - b;
-    if (operator === 'multiply') return a * b;
-    if (operator === 'divide') return b==0?'Infinite':a / b;
-}
-// action type
-const getKeyType = key => {
-    const { action } = key.dataset
-    if (!action) return 'number'
-    // if action one of the main operators
-    if (
-      action === 'add' ||
-      action === 'subtract' ||
-      action === 'multiply' ||
-      action === 'divide'
-    ) return 'operator'
-    // For everything else, return the action
-    return action;
-}
-const readyResult = (key, numInDisplay, state) => {
-    const keyType = getKeyType(key);
-    let dispNum = numInDisplay; 
 
-    if (numInDisplay === '' ){
-        
+var stringResult = '';
+const readyResult = (key) => {
+    switch(key.textContent){
+        case '=':
+            stringResult = display.textContent + key.textContent;
+            let p = eval(stringResult);
+            display.textContent = p;
+            break;
+        case 'AC':
+            stringResult = '';
+            display.textContent = stringResult;
+            break;
+        default: 
+            stringResult = display.textContent + key.textContent;
+            display.textContent += key.textContent;
+            break;
     }
+    
+
 }
 
 const calculator = document.querySelector('.container');
@@ -36,10 +27,8 @@ const keys = calculator.querySelector('.buttons');
 keys.addEventListener('click', e => {
     if (!e.target.matches('button')) return
     const key = e.target
-    const numInDisplay= display.textContent;
-    const resultString = readyResult(key, numInDisplay, calculator.dataset);
-  
-    display.textContent = resultString;
-    updateCalculator(key, calculator, resultString, numInDisplay);
-    updateVisualState(key, calculator);
+    
+
+    readyResult(key);
+    
 })
